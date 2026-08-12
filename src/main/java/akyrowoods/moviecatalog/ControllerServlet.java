40b20722c13b8a106cd29b8e1b36c7fe55f69cbd
Akyro Woods <akyrowoods07@gmail.com>
2026-08-12 13:53:49 -0700

    Implemented servlet methods for functionality.

---
+++ src/main/java/akyrowoods/moviecatalog/ControllerServlet.java
@@ -0,0 +1,127 @@
+package akyrowoods.moviecatalog;
+
+
+import java.io.IOException;
+import java.io.Serial;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet({"/new", "/insert", "/update", "/edit", "/delete", "/list", "/details"})
+public class ControllerServlet extends HttpServlet{
+    @Serial
+    private static final long serialVersionUID = 1L;
+    private MovieDao movieDao;
+    private MovieApiClient api;
+    private JsonToMovie movieConverter;
+
+    public void init() {
+        String jdbcUrl = getServletContext().getInitParameter("jdbcUrl");
+        String jdbcUsername = getServletContext().getInitParameter("jdbcUsername");
+        String jdbcPassword = getServletContext().getInitParameter("jdbcPassword");
+        movieDao = new MovieDao(jdbcUrl, jdbcUsername, jdbcPassword);
+
+        api = new MovieApiClient();
+        movieConverter = new JsonToMovie();
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getServletPath();
+        try {
+            switch (action) {
+                case "/new" -> showNewForm(request, response);
+                case"/insert" -> insertMovie(request, response);
+                case"/details" -> showMovieDetails(request,response);
+                case"/update" -> updateMovie(request, response);
+                case"/edit" -> showEditForm(request, response);
+                case"/delete" -> deleteMovie(request,response);
+                default -> listAllMovies(request, response);
+            }
+        } catch (Exception e) {
+            throw new ServletException(e);
+        }
+    }
+
+    private void listAllMovies(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Movie> movieCollection = movieDao.listAllMovies();
+        for(Movie m: movieCollection) {
+            List<String> movieGenres = movieDao.gatherGenres(m);
+            m.setGenreList(movieGenres);
+        }
+        System.out.println("Movies loaded: " + movieCollection.size());
+        request.setAttribute("movies", movieCollection);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("Collection.jsp");
+        dispatcher.forward(request,response);
+    }
+    private void showNewForm(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("AddMovie.jsp");
+        dispatcher.forward(request,response);
+    }
+
+    public void showEditForm(HttpServletRequest request, HttpServletResponse response) throws SQLException, ServletException, IOException {
+
+        int movieId = Integer.parseInt(request.getParameter("movieId"));
+        Movie movie = movieDao.getMovieById(movieId);
+        List<String> allGenres = movieDao.listAllGenres();
+        List<String> movieGenres = movieDao.gatherGenres(movie);
+        movie.setGenreList(movieGenres);
+
+        request.setAttribute("movie", movie);
+        request.setAttribute("allGenres", allGenres);
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("EditMovie.jsp");
+        dispatcher.forward(request, response);
+    }
+    private void insertMovie(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String title = request.getParameter("title").replace(" ", "+");
+        String format = request.getParameter("format");
+        Movie movie = movieConverter.movieAssembler(api.fetchMovie(title), format);
+        System.out.println("Inserted movie: " + movie.getTitle());
+        movieDao.insertMovie(movie);
+        response.sendRedirect("list");
+    }
+
+    private void showMovieDetails(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int movieId = Integer.parseInt(request.getParameter("movieId"));
+        Movie movie = movieDao.getMovieById(movieId);
+        List<String> genre = movieDao.gatherGenres(movie);
+        movie.setGenreList(genre);
+        request.setAttribute("movie", movie);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("Details.jsp");
+        dispatcher.forward(request,response);
+    }
+
+    private void updateMovie(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        int movieId = Integer.parseInt(request.getParameter("movieId"));
+        String title = request.getParameter("title");
+        String format = request.getParameter("format");
+        String[] genres = request.getParameterValues("genres");
+
+        Movie movie = movieDao.getMovieById(movieId);
+        movie.setTitle(title);
+        movie.setFormat(Formats.valueOf(format));
+        movie.setGenreList(Arrays.stream(genres).toList());
+        movieDao.updateMovie(movie);
+
+        response.sendRedirect("list");
+    }
+
+    private void deleteMovie(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        int movieId = Integer.parseInt(request.getParameter("movieId"));
+        Movie movie = movieDao.getMovieById(movieId);
+        movieDao.deleteMovie(movie);
+        response.sendRedirect("list");
+    }
+}
