7d4d58de3327de655fdbbf122e3174fe9e718b28
Akyro Woods <akyrowoods07@gmail.com>
2026-08-12 13:52:52 -0700

    Added title sanitization to api call.

--- src/main/java/akyrowoods/moviecatalog/MovieApiClient.java
+++ src/main/java/akyrowoods/moviecatalog/MovieApiClient.java
@@ -7,15 +7,18 @@ import java.net.http.HttpResponse;
 
 public class MovieApiClient {
     private final HttpClient client;
+    private final JsonToMovie formatter;
 
     public MovieApiClient() {
-         this.client = HttpClient.newHttpClient();
+        this.client = HttpClient.newHttpClient();
+        this.formatter = new JsonToMovie();
     }
 
     public String fetchMovie(String title) {
+        String sanitizedTitle = formatter.sanitizeMovieTitle(title);
         String apiResponse = "";
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://www.omdbapi.com/?i=tt3896198&apikey=3c4dc7f5" + "&t=" + title))
+                .uri(URI.create("http://www.omdbapi.com/?i=tt3896198&apikey=3c4dc7f5" + "&t=" + sanitizedTitle))
                 .GET().build();
 
         try {
@@ -25,11 +28,8 @@ public class MovieApiClient {
             System.out.println("Error Occurred: " + e.getMessage());
 
         }
-    return apiResponse;
+        return apiResponse;
     }
-
-
-
 
 
 }
